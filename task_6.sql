4e691795855e9469dc9a329b5ef7bac8587f71c9
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-08-13 17:02:41 +0100

    typo

--- task_6.sql
+++ task_6.sql
@@ -13,9 +13,9 @@ try:
     # Query column details from INFORMATION_SCHEMA
     query = """
       INSERT INTO customer (customer_id, customer_name, email, address)
-      VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-      (3, 'Obed Ehoneah', 'cbaidoo@sandtech.com', '125 Happiness Ave.'),
-       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+      VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+      (3, 'Obed Ehoneah', 'cbaidoo@sandtech.com', '125 Happiness  Ave.'),
+       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
       
 """
     cursor.execute("USE alx_book_store;")
