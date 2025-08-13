1ea66fac5210eb1387ab106ea67c3e52fdf0f94a
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-08-13 12:59:10 +0100

    DDL

---
+++ alx_book_store.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id BIGINT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(130) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    customer_id BIGINT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid BIGINT PRIMARY KEY AUTO_INCREMENT,
+    order_id BIGINT,
+    book_id BIGINT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
