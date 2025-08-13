50db1b8f595b1c034bf1cb455207e33c8bed9674
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-08-13 14:36:15 +0100

    task 3

--- task_3.sql
+++ task_3.sql
@@ -11,9 +11,18 @@ try:
     if connection.is_connected():
         cursor = connection.cursor()
         # Create database and select it
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        cursor.execute("USE alx_book_store")
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+        cursor.execute("USE alx_book_store;")
         # List tables in DATABASE
-        cursor.execute("SHOW TABLES")
+        cursor.execute("SHOW TABLES;")
         tables = cursor.fetchall()
         print("Tables in the database:", tables)
+except mysql.connector.Error as e:
+    print("Error:", e)
+else:
+    print("Database 'alx_book_store' created successfully.")
+finally:
+    if 'cursor' in locals() and cursor:
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
